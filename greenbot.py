a1eda59c49beabb8d7ab2b23d79d373c82b6c96d
Quytelda Gaiwin <admin@tamalin.org>
2014-10-06 01:42:56 -0500

    Fixed missing import statement.

--- greenbot.py
+++ greenbot.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with greenbot.  If not, see <http://www.gnu.org/licenses/>.
 
-import time
+import time, re
 import sys
 import os
 
