34502ae1925586ab8e1bfe28878159c64fe1e6cd
Quytelda Gaiwin <admin@tamalin.org>
2014-11-06 16:20:09 -0600

    Add voting module (in progress).

---
+++ modules/voting.py
@@ -0,0 +1,27 @@
+#
+# modules/voting.py - Module to allow channel management by voting
+# This module is essential for greenbot.  Do not remove it or things won't work!
+#
+# Copyright (C) 2014 Quytelda Gaiwin <admin@tamalin.org>
+#
+# This file is part of greenbot, the python IRC logging bot.
+#
+# greenbot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# greenbot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with greenbot.  If not, see <http://www.gnu.org/licenses/>.
+
+actions = {"" : "",}
+motions = {}
+
+cast_vote(nick, motion, cast = True):
+	if cast:
+		
